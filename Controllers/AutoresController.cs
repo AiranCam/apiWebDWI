7f38e67c48bd396c37237d6d96f497ba7cb20ea0
AiranCam <camposairan800@gmail.com>
2024-06-11 18:35:00 -0500

    feat: upstream local changes into remote repository

---
+++ Controllers/AutoresController.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebApi92.Services;
+
+namespace WebApi92.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AutoresController : Controller
+    {
+        private readonly IAutorServices _autorServices;
+        public AutoresController(IAutorServices autorServices)
+        {
+            _autorServices = autorServices;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAutores()
+        {
+            return Ok(await _autorServices.GetAutores());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> AutorById(int id)
+        {
+            return Ok(await _autorServices.GetAutorById(id));
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CrearAutor([FromBody]Autor autor)
+        {
+            return Ok(await _autorServices.CreateAutor(autor));
+        }
+
+
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> EditAutor([FromBody] Autor autor, int id)
+        {
+            return Ok(await _autorServices.EditarAutor(autor, id));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> EliminarAutor(int id)
+        {
+            return Ok(await _autorServices.EliminarAutor(id));
+        }
+    }
+}
